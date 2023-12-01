006b1385c648119e4b05918ac9c404d986737349
Daniel Qu <danielq987@gmail.com>
2023-12-01 11:34:37 -0500

    Add readme and clean up day1

---
+++ README.md
@@ -0,0 +1,5 @@
+# advent-of-code-2023
+
+Everything is written in Python. I'll try to clean up the code somewhat after each day so that it's readable.
+
+I'll try to go for as many days as possible, but typically I tap about after about 12 days. Maybe I'll make top 100 on one of the days if I try really hard :D
--- solutions/day1.py
+++ solutions/day1.py
@@ -22,25 +22,25 @@ def convert_to_number(num_str):
 
 def main():
     lines = readFile("day1.txt")
-    
-    # Do some data processing here
-    lines = [line for line in lines]
 
-    s = 0
+    part1 = 0
+    part2 = 0
 
+    # Build the regex
     number_words = '|'.join(list(lookup.keys()))
-
     nongreedy = f"^.*?(\d|{number_words})"
     greedy = f"^.*(\d|{number_words})"
-    for line in lines:
-        num1 = re.findall(nongreedy, line)
-        num2 = re.findall(greedy, line)
-        value = int(convert_to_number(num1[0])*10+convert_to_number(num2[0]))
-        s += value
 
-    print(s) 
-    
-    # print(re.findall(reString, "oneight", overlapped=True))
+    for line in lines:
+        digits = re.findall('\d', line)
+        part1 = int(convert_to_number(digits[0])*10+convert_to_number(digits[1]))
+
+        num1 = re.findall(nongreedy, line) # Non-greedy gets the first match
+        num2 = re.findall(greedy, line) # Greedy gets the last match
+        part2 += int(convert_to_number(num1[0])*10+convert_to_number(num2[0]))
+
+    print(part1)
+    print(part2)
     
 if __name__ == "__main__":
     main()
